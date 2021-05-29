fc273d706e6402bb749bd0c37f3dcc01539e92ea
aadiyat.ahmad <aadiyat.ahmad@gmail.com>
2021-05-28 21:39:28 -0400

    Created top level component, Screen

---
+++ src/components/Screen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Screen extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    render(){
+        return (<div>Placeholder.</div>);
+    }
+}
+
+export default Screen;
