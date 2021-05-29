ff757b970cb21c9e35a1ec51f0a85e45ccfdca37
aadiyat.ahmad <aadiyat.ahmad@gmail.com>
2021-05-28 21:43:31 -0400

    created object of game parameters

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+//TODO: Currently hard coding parameters.
+// wordLength will depend on difficulty
+// read specialChars and words from json
+const gameParameters = {
+    wordLength : 7,
+    wordSpacing: 2, // Minimum spacing between words
+    words: ["dangers", "sending", "central", "hunters", "resides", "believe", "venture", "pattern", "discard", "mention", "cutters", "canteen", "beliefs", "banning", "minigun", "cistern"],
+    specialChars : [',','.','!','@','#','$','%','&','(',')','{','}','[',']','<','>','?','"',"'", '/', '|'],
+    charStringLength: 408,
+}
+
 class Screen extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            
+
         };
     }
 
