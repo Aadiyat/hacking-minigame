e5c8ccaa6ed1ec5c96138437f288131705d2a725
Aadiyat <aadiyat.ahmad@gmail.com>
2021-06-14 22:54:46 -0400

    complete previous rename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Game from './components/Screen';
+import Game from './components/Game';
 import Welcome from './components/Welcome';
 
 function App() {
